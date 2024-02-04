96a46d603623c3429150ff48ff6982b7f863e94f
3Dima10 <112176437+3Dima10@users.noreply.github.com>
2024-02-04 16:42:57 +0200

    Delete pcp directory

--- pcp/main/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Dolw
-
-# Register your models here.
-admin.site.register(Dolw)
--- pcp/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="home"),
-    path("polo", views.index2, name="pol"),
-    path("cot", views.cot, name="cot"),
-]
